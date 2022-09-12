20f48adceab8ced3ecb6373b77b7e34e6be1ab21
EvedoesCS <leonardeve03@gmail.com>
2022-09-12 14:03:21 -0600

    Feat: Gladiator class created The class Gladiator now exists and has five properties Name, id, health, strength, and defense.

---
+++ gladiatorClass.py
@@ -0,0 +1,11 @@
+class gladiator():
+    def __init__(self, name, id, health, strength, defense):
+        # Personal Info
+        self.name = name
+        self.id = id
+        
+        # Stats Attributes
+        self.health = health
+        self.strength = strength
+        self.defense = defense
+
