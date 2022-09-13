3b91fc30230eec65b0346d5753b00472211c9d23
EvedoesCS <leonardeve03@gmail.com>
2022-09-12 22:32:37 -0600

    Feat: simulateRound now simulated 1v1 The simulateRound() function now takes two gladiators as arguments and returns the winner of combat.

--- main.py
+++ main.py
@@ -17,3 +17,52 @@ for i in range(0, tSize):
     newGladiator = gladiator(names[nameIndex], i, 100, 5, 5)
     combatants.append(newGladiator)
 
+
+def calcDmg(attacker: gladiator, defender: gladiator):
+        # Calculates attackers power
+        strMultiplier = random.randint(0, 20)
+        atkStr = attacker.strength * strMultiplier
+
+        # Calculates defenders power
+        defMultiplier = random.randint(0, 20)
+        defStr = defender.defense * defMultiplier
+
+        # Total damage inflicted, value cannot be less than zero
+        damageDone = atkStr - defStr
+        if damageDone < 0:
+            damageDone = 0
+        
+        return damageDone
+
+# Function defines a single "turn" of combat
+def fightTurn(gladiatorA: gladiator, gladiatorB: gladiator):
+    # Turn one, gladiatorA attacks, B defends
+    gladiatorB.health -= calcDmg(gladiatorA, gladiatorB)
+    if gladiatorB.health <= 0:
+        return "A"
+
+    # Turn Two, gladiatorB attacks, A defends 
+    gladiatorA.health -= calcDmg(gladiatorB, gladiatorA)
+    if gladiatorA.health <= 0:
+        return "B"
+    else:
+        return
+
+# Function defines a single match consisting of "turns" between gladiators
+def simulateRound(gladiatorA: gladiator, gladiatorB: gladiator):
+    # While neither combatant is dead ...aka health > 0
+    winner = None
+    while True:
+        returnVal = fightTurn(gladiatorA, gladiatorB)
+        if returnVal == "A":
+            winner = gladiatorA.name
+            break
+        elif returnVal == "B":
+            winner = gladiatorB.name
+            break
+        
+    return winner
+            
+print(simulateRound(combatants[0], combatants[1]))
+        
+
